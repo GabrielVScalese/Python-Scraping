4de876889288cb7982000ddf0c06daea6adb72d7
gabriel scalese <cc19171@g.unicamp.br>
2021-06-20 18:22:05 -0300

    Updated project

--- Search.py
+++ Search.py
@@ -19,46 +19,30 @@ class Search:
         self.br.form['senha'] = password
         self.br.submit()
     
+    def get_grades (self, courseCode):
+      self.br.open(f'https://sae.cotuca.unicamp.br/alunos/{self.ra}/boletim?utf8=%E2%9C%93&Ano={self.year}&curso_aluno={courseCode}&commit=Buscar')
+
+      soup = BeautifulSoup(self.br.response().read(), 'html.parser')
+
+      grade_table = soup.find_all('table', class_='table table-striped table-bordered')[1]
+
+      lines = grade_table.find_all('tr')
+
+      del lines[0]
+      del lines[0]
+
+      classes = []
+
+      # 2 -> name / 4 -> grade
+      for line in lines:
+        sub_lines = line.find_all('td')
+
+        classes.append(Class(sub_lines[2].text.strip(), sub_lines[4].text, sub_lines[5].text, sub_lines[8].text, sub_lines[9].text))
+
+      return classes 
+    
     def get_technician_grade(self):
-        self.br.open(f'https://sae.cotuca.unicamp.br/alunos/{self.ra}/boletim?utf8=%E2%9C%93&Ano={self.year}&curso_aluno=28&commit=Buscar')
-
-        soup = BeautifulSoup(self.br.response().read(), 'html.parser')
-
-        grade_table = soup.find_all('table', class_='table table-striped table-bordered')[1]
-
-        lines = grade_table.find_all('tr')
-
-        del lines[0]
-        del lines[0]
-
-        classes = []
-
-        # 2 -> name / 4 -> grade
-        for line in lines:
-          sub_lines = line.find_all('td')
-
-          classes.append(Class(sub_lines[2].text.strip(), sub_lines[4].text, sub_lines[5].text, sub_lines[8].text, sub_lines[9].text))
-
-        return classes
+      return self.get_grades(28)
 
     def get_high_school_grade(self):
-        self.br.open(f'https://sae.cotuca.unicamp.br/alunos/{self.ra}/boletim?utf8=%E2%9C%93&Ano={self.year}&curso_aluno=78&commit=Buscar')
-        
-        soup = BeautifulSoup(self.br.response().read(), 'html.parser')
-
-        grade_table = soup.find_all('table', class_='table table-striped table-bordered')[1]
-
-        lines = grade_table.find_all('tr')
-
-        del lines[0]
-        del lines[0]
-
-        classes = []
-
-        # 2 -> name / 4 -> grade
-        for line in lines:
-            sub_lines = line.find_all('td')
-
-            classes.append(Class(sub_lines[2].text.strip(), sub_lines[4].text, sub_lines[5].text, sub_lines[8].text, sub_lines[9].text))
-
-        return classes
+      return self.get_grades(78)
